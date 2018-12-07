c121cda063ebdcaea591175413421018634acbfd
Tanin Na Nakorn <tanin47@users.noreply.github.com>
2018-12-07 14:58:07 -0800

    Remove await and timeout from Worker

--- build.sbt
+++ build.sbt
@@ -14,7 +14,7 @@ libraryDependencies ++= Seq(
 
 organization := "givers.moonlight"
 name := "play-moonlight"
-version := "0.1.2"
+version := "0.1.3"
 parallelExecution in Test := false
 
 publishMavenStyle := true
--- src/main/scala/givers/moonlight/Worker.scala
+++ src/main/scala/givers/moonlight/Worker.scala
@@ -10,10 +10,6 @@ import scala.concurrent.{Await, Future}
 
 abstract class Worker[Param <: Job](implicit jsonFormat: OFormat[Param]) {
 
-  val DEFAULT_FUTURE_TIMEOUT = Duration.apply(30, TimeUnit.SECONDS)
-
-  protected[this] def await[T](future: Future[T]): T = Await.result(future, DEFAULT_FUTURE_TIMEOUT)
-
   def run(job: BackgroundJob): Unit = {
     run(
       param = jsonFormat.reads(Json.parse(job.paramsInJsonString)).get,
